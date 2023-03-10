41e0c18052d2a5bd617ecb3a43bdf01f802edc4d
IJOLZT <h880824@stud.u-szeged.hu>
2023-03-10 14:56:57 +0100

    in progress

--- mock/mock_script.py
+++ mock/mock_script.py
@@ -1,16 +1,18 @@
 from faker import Faker
 from coolname import generate
+from wonderwords import RandomWord
 import random
 faker = Faker(locale='en_US')
-
+r = RandomWord()
 
 def generate_Publisher():
     print(faker.phone_number())
     print(faker.address())
     print(faker.company())
 
-def gen_genres(book_isbn:int):
+def gen_genres(book_isbn):
     genres = ["Fantasy","Adventure","Romance","Contemporary","Dystopian","Mystery","Horror","Thriller","Paranormal","Historical fiction","Science Fiction","Children’s" ,"Memoir","Cookbook","Art","Self-help","Development","Motivational","Health","History","Travel","Guide / How-to","Families & Relationships","Humor"]
+    sql = f"INSERT INTO mufaja (mufajnev, isbn) VALUES ({genres[random.randint(0,len(genres)-1)]}, {book_isbn});"
 
 def author():
     print(faker.name())
@@ -24,8 +26,7 @@ def user():
     s_name = faker.last_name()
     email = f_name+'_'+s_name+'@gmail.com'
     passwd = faker.word()
-    print(faker.address())
-    print(faker.phone_number())
+    sql = f"INSERT INTO felhasznalo (email, nev, jelszo, cim, telefon, szerep) VALUES ({email}, {f_name+' '+s_name}, {passwd}, {faker.address(), {faker.phone_number()}, {role[random.randint(0, len(role)-1)]}});"
 
 
 def book():
@@ -33,22 +34,23 @@ def book():
     title.capitalize()
     cover = ["soft-cover","hard-cover", "ebook"]
     price = faker.random_number(digits=4)
-    isbn = faker.random_number(digits=9)
     page_number = faker.random_number(digits=3)
-    sql = f"INSERT INTO konyv (isbn, boritokep, ar, kotes, cim, oldalszam) VALUES ({isbn}, {None}, {price}, {cover[random.randint(0, 2)]}, {title}, {page_number});"
+    sql = f"INSERT INTO konyv (isbn, boritokep, ar, kotes, cim, oldalszam) VALUES ({faker.isbn10()}, {None}, {price}, {cover[random.randint(0, len(cover)-1)]}, {title}, {page_number});"
     
 
-def who_and_when_published(comp_name:str, book_isbn:int):
-    faker.date()
+def who_and_when_published(comp_name:str, book_isbn, book_date):
+    sql = f"INSERT INTO kiadta (nev, isbn, mikor) VALUES ({comp_name},{book_isbn}, {book_date});"
 
-def author_of_book(auth_name:str, book_isbn:int):
+def author_of_book(auth_name:str, book_isbn):
     pass
 
-def favourite(book_isbn:int, mail:str):
-    pass
+def favourite(book_isbn, mail:str):
+    sql = f"INSERT INTO kedvence (isbn, email) VALUES ({book_isbn}, {mail});"
 
-def rating(book_isbn:int, mail:str,rate:int, text:str):
-    pass
+def rating(book_isbn, mail:str):
+    sql = f"INSERT INTO ertekeles (isbn, email, pontszam, megjegyzes) VALUES ({book_isbn}, {mail}, {random.randint(1,6)}, {r.word( include_parts_of_speech=['nouns', 'adjectives'])});"
 
 def cart():
     pass
+
+
