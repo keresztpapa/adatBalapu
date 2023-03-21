8bed867fb0db6b8f56e699d79cac32a112c77528
keresztpapa <48327504+keresztpapa@users.noreply.github.com>
2023-03-21 13:59:39 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-const port = 8080
+const port = 5000;
 const express = require('express');
+const oracledb = require('oracledb');
 const {resolve} = require('path');
 const app = express();
 
@@ -13,5 +14,28 @@ const index = resolve('../frontend/html/index.html');
 
 app.get('/', (req, res) => res.sendFile(index));
 app.get('/index', (req, res) => res.sendFile(index));
+
+async function run() {
+    try {
+      connection = await oracledb.getConnection({
+        user: "SCOTT",
+        password: "tiger",
+        connectString : "localhost:1521/XE"
+      });
+      result = await connection.execute(`SELECT * FROM kedvence`);
+      console.log("Result is:", result);
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err.message);
+        }
+      }
+    }
+}
+run();
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
