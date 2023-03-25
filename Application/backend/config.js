4fccf91b67a0a2bcce3e2939a05de0a60774d376
Fehér Konrád <h157451@stud.u-szeged.hu>
2023-03-25 14:44:01 +0100

    config file, jön a .gitignore mindj

---
+++ Application/backend/config.js
@@ -0,0 +1,11 @@
+const getConfig = () => {
+    return {
+        'ssh_username' : 'h123456',
+        'ssh_password' : 'strong_password123',
+        'db_username' : 'C##NPT4LF',
+        'db_password' : 'not_so_strong_password123'
+    } 
+    
+};
+
+exports.getConfig = getConfig;
