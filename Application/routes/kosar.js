9c3910344d9554fb1846ae4a4fe6b7f13c0d26e9
IJOLZT <h880824@stud.u-szeged.hu>
2023-04-24 11:33:24 +0200

    insertion is not yet copmplete, cart can be listed

--- Application/routes/kosar.js
+++ Application/routes/kosar.js
@@ -11,7 +11,6 @@ router.get('/', async (req, res) => {
     const conn = await getConnection();
 
     const result = await conn.client.execute(`SELECT id, email, isbn, darabszam, hova FROM tetel`);
-
     res.render('kosar', { data: result.rows });
   } catch (err) {
     console.error(err);
@@ -20,18 +19,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/kosar', async (req, res) => {
-/*  
-  try {
-    const conn = await getConnection();
 
-    const result = await conn.client.execute(`SELECT id, email, isbn, darabszam, hova FROM tetel`);
-
-    res.render('kosar', { data: result.rows });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error 1');
-  }
-  */
 });
 
 module.exports = router;
