1d474e02fdd1c8361b4b228827a97cd2ca30540f
KonradFeher <konradfeher@outlook.com>
2023-04-25 21:49:17 +0200

    triggers tested and working

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -9,55 +9,40 @@ BEGIN
 END;
 /
 
--- kitöröljük a törlendő felhasználó tételeit is
-CREATE OR REPLACE TRIGGER trg_delete_tetel_on_felhasznalo_delete
-BEFORE DELETE ON felhasznalo
-FOR EACH ROW
-BEGIN
-  DELETE FROM tetel
-  WHERE email = :OLD.email;
-END;
-/
-
--- kiadta tábla frissítése on könyv insert/delete
-CREATE OR REPLACE TRIGGER update_kiadta
-AFTER INSERT OR DELETE ON konyv
-FOR EACH ROW
-DECLARE
-BEGIN
-  IF INSERTING THEN
-    INSERT INTO kiadta (nev, isbn, mikor)
-    VALUES (:NEW.cim, :NEW.isbn, EXTRACT(YEAR FROM SYSDATE));
-  ELSE
-    DELETE FROM kiadta
-    WHERE isbn = :OLD.isbn;
-  END IF;
-END;
-/
-
 -- mufaj tábla frissítése on könyv insert ('ismeretlen')/delete
-CREATE OR REPLACE TRIGGER tie_mufaj_trigger
-AFTER INSERT OR DELETE ON konyv
+CREATE OR REPLACE TRIGGER add_ismeretlen_mufaj_trigger
+AFTER INSERT ON konyv
 FOR EACH ROW
-DECLARE
-  mufaj mufaja.mufajnev%TYPE;
 BEGIN
-  IF INSERTING THEN
-    INSERT INTO mufaja (mufajnev, isbn) VALUES ('ismeretlen', :NEW.isbn);
-  END IF;
-  
-  IF DELETING THEN
-    DELETE FROM mufaja WHERE isbn = :OLD.isbn;
-  END IF;
+  INSERT INTO mufaja (mufajnev, isbn) VALUES ('ismeretlen', :NEW.isbn);
 END;
 /
 
 -- ismeretlen mufaj torlese ha adunk hozza mas mufajt
-CREATE OR REPLACE TRIGGER remove_ismeretlen_mufaja
+CREATE OR REPLACE TRIGGER remove_ismeretlen_mufaja_trigger
 BEFORE INSERT ON mufaja
 FOR EACH ROW
 BEGIN
   DELETE FROM mufaja
   WHERE mufajnev = 'ismeretlen' AND isbn = :NEW.isbn;
+END;
+/
+
+-- szerzoje tábla frissítése on könyv insert ('ismeretlen')/delete
+CREATE OR REPLACE TRIGGER add_ismeretlen_szerzo_trigger
+AFTER INSERT ON konyv
+FOR EACH ROW
+BEGIN
+  INSERT INTO szerzoje (szerzonev, isbn) VALUES ('ismeretlen', :NEW.isbn);
+END;
+/
+
+-- ismeretlen szerzo torlese ha adunk hozza mas szerzot
+CREATE OR REPLACE TRIGGER remove_ismeretlen_szerzoje_trigger
+BEFORE INSERT ON szerzoje
+FOR EACH ROW
+BEGIN
+  DELETE FROM szerzoje
+  WHERE szerzonev = 'ismeretlen' AND isbn = :NEW.isbn;
 END;
 
