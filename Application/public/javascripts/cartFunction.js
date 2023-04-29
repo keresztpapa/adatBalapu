3e839416024816354e48b4ebd769efbb748d1bec
IJOLZT <h880824@stud.u-szeged.hu>
2023-04-29 18:09:38 +0200

    fizetés done

--- Application/public/javascripts/cartFunction.js
+++ Application/public/javascripts/cartFunction.js
@@ -55,15 +55,43 @@ function deleteFromCart(id) {
 
 function finalize() {
   var popup = window.open("", "popup", "width=400,height=400");
-  popup.document.write("<form onsubmit='submitForm()'>"); 
+  popup.document.write("<html><head><title>Payment Details</title>");
+  popup.document.write("<style>");
+  popup.document.write("form {text-align: center;}");
+  popup.document.write(`label {
+                          display: inline-block; 
+                          width: 120px; 
+                          text-align: right; 
+                          margin-right: 10px;
+                        }
+                        .popup-input {
+                          margin-bottom: 10px;
+                        }
+                        .popup-form {
+                          padding: 10px;
+                        }
+                        `);
+  popup.document.write("input[type='text'] {width: 200px;}");
+  popup.document.write("input[type='submit'] {margin-top: 10px;}");
+  popup.document.write("</style></head><body>");
+  popup.document.write("<form class='popup-form' onsubmit='submitForm()'>");
   popup.document.write("<label for='cardNumber'>Kártyaszám:</label>");
-  popup.document.write("<input type='text' id='cardNumber' name='cardNumber'><br>");
+  popup.document.write("<input class='popup-input' type='text' id='cardNumber' name='cardNumber'><br>");
   popup.document.write("<label for='expiration'>Lejárati idő:</label>");
-  popup.document.write("<input type='text' id='expiration' name='expiration'><br>");
+  popup.document.write("<input class='popup-input' type='text' id='expiration' name='expiration'><br>");
   popup.document.write("<label for='cvv'>CVV kód:</label>");
-  popup.document.write("<input type='text' id='cvv' name='cvv'><br>");
-  popup.document.write("<input type='submit' value='Submit'>");
+  popup.document.write("<input class='popup-input' type='text' id='cvv' name='cvv'><br>");
+  popup.document.write("<input type='submit' value='Fizetés'>");
   popup.document.write("</form>");
+  popup.document.write(`<script> 
+                        function submitForm() {
+                          window.opener.alert("A fizetés sikeres volt!"); 
+                          window.close(); 
+                        } 
+                        </script>`);
+  popup.document.write("</body></html>");
+  popup.document.close();
+  popup.focus();
 }
 
 function submitForm() {
