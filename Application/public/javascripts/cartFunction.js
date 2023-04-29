b6660bab5dcc0b9b84c405ff5d4e9007452f85da
IJOLZT <h880824@stud.u-szeged.hu>
2023-04-29 18:20:29 +0200

    random uzletek

--- Application/public/javascripts/cartFunction.js
+++ Application/public/javascripts/cartFunction.js
@@ -54,7 +54,7 @@ function deleteFromCart(id) {
 
 
 function finalize() {
-  var popup = window.open("", "popup", "width=400,height=400");
+  var popup = window.open("", "popup", "width=400,height=450");
   popup.document.write("<html><head><title>Payment Details</title>");
   popup.document.write("<style>");
   popup.document.write("form {text-align: center;}");
@@ -75,12 +75,21 @@ function finalize() {
   popup.document.write("input[type='submit'] {margin-top: 10px;}");
   popup.document.write("</style></head><body>");
   popup.document.write("<form class='popup-form' onsubmit='submitForm()'>");
+  
   popup.document.write("<label for='cardNumber'>Kártyaszám:</label>");
   popup.document.write("<input class='popup-input' type='text' id='cardNumber' name='cardNumber'><br>");
   popup.document.write("<label for='expiration'>Lejárati idő:</label>");
   popup.document.write("<input class='popup-input' type='text' id='expiration' name='expiration'><br>");
   popup.document.write("<label for='cvv'>CVV kód:</label>");
   popup.document.write("<input class='popup-input' type='text' id='cvv' name='cvv'><br>");
+  
+  popup.document.write("<label for='address'>Cím:</label>");
+  popup.document.write("<select class='popup-input' id='address' name='address'>");
+  popup.document.write("<option value='123 Main St'>123 Main St</option>");
+  popup.document.write("<option value='456 Oak Ave'>456 Oak Ave</option>");
+  popup.document.write("<option value='789 Maple Dr'>789 Maple Dr</option>");
+  popup.document.write("</select><br>");
+  
   popup.document.write("<input type='submit' value='Fizetés'>");
   popup.document.write("</form>");
   popup.document.write(`<script> 
